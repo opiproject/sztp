028a4f870c3c5a73a37ea7bef11e381c3a2871de
Alberto Morgante Medina <alknopfler@users.noreply.github.com>
2022-10-10 12:54:53 +0200

    Define the agent structure fields
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- sztp-agent/pkg/secureAgent/agent.go
+++ sztp-agent/pkg/secureAgent/agent.go
@@ -7,15 +7,47 @@ Copyright (C) 2022 Red Hat.
 
 package secureAgent
 
+//Agent is the basic structure to define an agent instance
 type Agent struct {
-	ConfigURL string
-	//TBD the rest of fields
+	BootstrapURL             string //Bootstrap complete URL
+	SerialNumber             string //Device's Serial Number
+	DevicePassword           string //Device's Password
+	DevicePrivateKey         string //Device's private key
+	DeviceEndEntityCert      string //Device's end-entity cert
+	BootstrapTrustAnchorCert string //the trusted bootstrap server's trust-anchor certificate (PEM)
 }
 
-func NewAgent(configURL string) *Agent {
-	return &Agent{ConfigURL: configURL}
+func NewAgent(bootstrapURL, serialNumber, devicePassword, devicePrivateKey, deviceEndEntityCert, bootstrapTrustAnchorCert string) *Agent {
+	return &Agent{
+		BootstrapURL:             bootstrapURL,
+		SerialNumber:             serialNumber,
+		DevicePassword:           devicePassword,
+		DevicePrivateKey:         devicePrivateKey,
+		DeviceEndEntityCert:      deviceEndEntityCert,
+		BootstrapTrustAnchorCert: bootstrapTrustAnchorCert,
+	}
 }
 
-func (a Agent) GetConfigURL() string {
-	return a.ConfigURL
+func (a *Agent) GetBootstrapURL() string {
+	return a.BootstrapURL
+}
+
+func (a *Agent) GetSerialNumber() string {
+	return a.SerialNumber
+}
+
+func (a *Agent) GetDevicePassword() string {
+	return a.DevicePassword
+}
+
+func (a *Agent) GetDevicePrivateKey() string {
+	return a.DevicePrivateKey
+}
+
+func (a *Agent) GetDeviceEndEntityCert() string {
+	return a.DeviceEndEntityCert
+}
+
+func (a *Agent) GetBootstrapTrustAnchorCert() string {
+	return a.BootstrapTrustAnchorCert
 }
