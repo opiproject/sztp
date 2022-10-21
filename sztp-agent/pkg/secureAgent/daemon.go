fa6c1187ce079792ab090ce52e5f29f542cd629b
Alberto Morgante <amorgant@redhat.com>
2022-10-21 09:35:41 +0200

    copy configuration and refactor pre and post function
    Signed-off-by: Alberto Morgante Medina <alknopfler@users.noreply.github.com>

--- sztp-agent/pkg/secureAgent/daemon.go
+++ sztp-agent/pkg/secureAgent/daemon.go
@@ -26,6 +26,11 @@ import (
 	"time"
 )
 
+const (
+	PRE  = "pre"
+	POST = "post"
+)
+
 func (a *Agent) RunCommandDaemon() error {
 	err := a.getBootstrapURL()
 	if err != nil {
@@ -39,11 +44,15 @@ func (a *Agent) RunCommandDaemon() error {
 	if err != nil {
 		return err
 	}
-	err = a.launchPreConfiguration()
+	err = a.copyConfigurationFile()
 	if err != nil {
 		return err
 	}
-	err = a.launchPostConfiguration()
+	err = a.launchScriptsConfiguration(PRE)
+	if err != nil {
+		return err
+	}
+	err = a.launchScriptsConfiguration(POST)
 	if err != nil {
 		return err
 	}
@@ -196,64 +205,67 @@ func (a *Agent) downloadAndValidateImage() error {
 	return nil
 }
 
-func (a *Agent) launchPreConfiguration() error {
-	log.Println("[INFO] Starting the Pre-Configuration.")
-	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + "preconfiguration.sh")
+func (a *Agent) copyConfigurationFile() error {
+	log.Println("[INFO] Starting the Copy Configuration.")
+	// Copy the configuration file to the device
+	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + "-config")
 	if err != nil {
-		log.Println("[ERROR] creating the  pre-configuration script", err.Error())
+		log.Println("[ERROR] creating the configuration file", err.Error())
 		return err
 	}
 	defer file.Close()
-	plainTest, err := base64.StdEncoding.DecodeString(a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PreConfigurationScript)
+	plainTest, err := base64.StdEncoding.DecodeString(a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.Configuration)
 	_, err = file.WriteString(string(plainTest))
 	if err != nil {
-		log.Println("[ERROR] writing the  pre-configuration script", err.Error())
+		log.Println("[ERROR] writing the configuration file", err.Error())
 		return err
 	}
-	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"preconfiguration.sh", 0755)
+	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"-config", 0744)
 	if err != nil {
-		log.Println("[ERROR] changing the  pre-configuration script permission", err.Error())
+		log.Println("[ERROR] changing the configuration file permission", err.Error())
 		return err
 	}
-	log.Println("[INFO] Pre-Configuration script created successfully")
-	cmd := exec.Command("/bin/sh", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"preconfiguration.sh")
-	out, err := cmd.Output()
-	if err != nil {
-		log.Println("[ERROR] running the  pre-configuration script", err.Error())
-		return err
-	}
-	log.Println(string(out)) //remove it
-	log.Println("[INFO] Pre-Configuration script executed successfully")
+	log.Println("[INFO] Configuration file copied successfully")
 	return nil
 }
 
-func (a *Agent) launchPostConfiguration() error {
-	log.Println("[INFO] Starting the Post-Configuration.")
-	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + "postconfiguration.sh")
+func (a *Agent) launchScriptsConfiguration(typeOf string) error {
+	var script, scriptName string
+	switch typeOf {
+	case "post":
+		script = a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PostConfigurationScript
+		scriptName = "post"
+		break
+	case "pre":
+		script = a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PreConfigurationScript
+		scriptName = "pre"
+	}
+	log.Println("[INFO] Starting the " + scriptName + "-configuration.")
+	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + scriptName + "configuration.sh")
 	if err != nil {
-		log.Println("[ERROR] creating the  post-configuration script", err.Error())
+		log.Println("[ERROR] creating the "+scriptName+"-configuration script", err.Error())
 		return err
 	}
 	defer file.Close()
-	plainTest, err := base64.StdEncoding.DecodeString(a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PostConfigurationScript)
+	plainTest, err := base64.StdEncoding.DecodeString(script)
 	_, err = file.WriteString(string(plainTest))
 	if err != nil {
-		log.Println("[ERROR] writing the  post-configuration script", err.Error())
+		log.Println("[ERROR] writing the "+scriptName+"-configuration script", err.Error())
 		return err
 	}
-	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"postconfiguration.sh", 0755)
+	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+scriptName+"configuration.sh", 0755)
 	if err != nil {
-		log.Println("[ERROR] changing the  post-configuration script permission", err.Error())
+		log.Println("[ERROR] changing the "+scriptName+"-configuration script permission", err.Error())
 		return err
 	}
-	log.Println("[INFO] Post-Configuration script created successfully")
-	cmd := exec.Command("/bin/sh", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"postconfiguration.sh")
+	log.Println("[INFO] " + scriptName + "-configuration script created successfully")
+	cmd := exec.Command("/bin/sh", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+scriptName+"configuration.sh")
 	out, err := cmd.Output()
 	if err != nil {
-		log.Println("[ERROR] running the  post-configuration script", err.Error())
+		log.Println("[ERROR] running the "+scriptName+"-configuration script", err.Error())
 		return err
 	}
 	log.Println(string(out)) //remove it
-	log.Println("[INFO] Post-Configuration script executed successfully")
+	log.Println("[INFO] " + scriptName + "-Configuration script executed successfully")
 	return nil
 }
