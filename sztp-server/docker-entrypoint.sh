0194454faff09420aa07a4a6aa4954068ce158e1
Boris Glimcher <Boris.Glimcher@emc.com>
2023-02-08 05:51:31 +0200

    sztpd: separate SZTPD_INIT_PORT vs SZTPD_NBI_PORT
    this is much mnore accurate for health check
    since after service restart port is changing and service is healthy
    only after restart with configuration
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- sztp-server/docker-entrypoint.sh
+++ sztp-server/docker-entrypoint.sh
@@ -5,7 +5,7 @@ wait_curl () {
     for i in $(seq 1 "${SZTPD_RETRY_ATTEMPTS:-10}")
     do
         echo "Attempt $i"
-        if curl --fail -H Accept:application/yang-data+json http://127.0.0.1:"${SZTPD_INIT_PORT}"/.well-known/host-meta
+        if curl --fail -H Accept:application/yang-data+json http://127.0.0.1:"${1}"/.well-known/host-meta
         then
             return 0
         else
@@ -18,19 +18,19 @@ wait_curl () {
 env
 
 # shellcheck disable=SC2016
-envsubst '$SZTPD_INIT_PORT,$SZTPD_SBI_PORT,$SZTPD_INIT_ADDR,$BOOTSVR_PORT,$BOOTSVR_ADDR' < /tmp/"${SZTPD_OPI_MODE}".json.static > /tmp/running.json
+envsubst '$SZTPD_INIT_PORT,$SZTPD_NBI_PORT,$SZTPD_SBI_PORT,$SZTPD_INIT_ADDR,$BOOTSVR_PORT,$BOOTSVR_ADDR' < /tmp/"${SZTPD_OPI_MODE}".json.static > /tmp/running.json
 diff /tmp/"${SZTPD_OPI_MODE}".json.static /tmp/running.json || true
 
 echo "starting server in the background"
 sztpd sqlite:///:memory: 2>&1 &
 
 echo "waiting for server to start"
-wait_curl
+wait_curl "${SZTPD_INIT_PORT}"
 
 echo "sending configuration file to server"
 curl -i -X PUT --user my-admin@example.com:my-secret --data @/tmp/running.json -H 'Content-Type:application/yang-data+json' http://127.0.0.1:"${SZTPD_INIT_PORT}"/restconf/ds/ietf-datastores:running
 
 echo "waiting for server to re-start"
-wait_curl
+wait_curl "${SZTPD_NBI_PORT}"
 
 wait
