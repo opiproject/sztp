5146cfa20182666c3ff58a58c100dc55ab6a7d6d
Xeckt <jordansavell1198@gmail.com>
2024-06-24 16:38:53 +0100

    add new systemd function to create systemd unit file and cobra command
    Signed-off-by: Xeckt <jordansavell1198@gmail.com>

---
+++ sztp-agent/pkg/secureagent/systemd_test.go
@@ -0,0 +1,44 @@
+package secureagent
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestCreateUnitFile(t *testing.T) {
+	const testOptions = "-bogus -options"
+	const unitFilePath = "/tmp"
+
+	contents := fmt.Sprintf(`[Unit]
+Description=SZTP Agent
+After=network.target
+
+[Service]
+ExecStart=sztp-agent %[1]s
+ExecReload=sztp-agent %[1]s
+Type=notify
+Restart=always
+
+[Install]
+WantedBy=default.target
+RequiredBy=network.target
+`, testOptions)
+
+	CreateUnitFile(testOptions, unitFilePath)
+
+	b, err := os.ReadFile(unitFilePath + "/" + unitFile)
+	if err != nil {
+		t.Errorf("Error reading unit file %s: %v", unitFilePath, err)
+	}
+
+	if !bytes.Equal(b, []byte(contents)) {
+		t.Errorf("Bytes do not match for contents and written unit file %s", unitFilePath)
+	}
+
+	err = os.Remove(unitFilePath + "/" + unitFile)
+	if err != nil {
+		t.Errorf("Error deleting unit file %s: %v", unitFilePath, err)
+	}
+}
