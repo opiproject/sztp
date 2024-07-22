edbf2c43bd3e467e11d236ca30bd6ab0829d18e1
Boris Glimcher <Boris.Glimcher@emc.com>
2024-07-22 21:47:30 +0300

    ci: prepare folder structure for dhcp feature
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ sztp-agent/pkg/dhcp/dbus.go
@@ -0,0 +1,9 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+Copyright (C) 2022-2023 Intel Corporation
+Copyright (c) 2022 Dell Inc, or its subsidiaries.
+Copyright (C) 2022 Red Hat.
+*/
+
+// Package dhcp implements the DHCP client
+package dhcp
